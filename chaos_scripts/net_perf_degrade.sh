6631771e44ac4b2a68a0ca05444f3505f8007a1a
rajibr <82666308+rajibrajkhowa@users.noreply.github.com>
2023-08-11 22:50:57 +0530

    Update

--- chaos_scripts/net_perf_degrade.sh
+++ chaos_scripts/net_perf_degrade.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+IFACE=$(nmcli device status | awk '{print $1}' | grep  '[eth|ens|enp]' | head -1)
 
 list=(1 2 3)
 
@@ -16,28 +17,25 @@ CORRUPT=(shuf -i 5-10 -n 1)
 if [ $x -eq 1 ]; then
   echo "Introducing packet loss of" "${LOSS}""%" "for" "${WAIT_PERIOD}""sec"
   echo
-  tc qdisc replace dev ens33 root netem loss ${LOSS}%
+  tc qdisc replace dev $IFACE root netem loss ${LOSS}%
   sleep $WAIT_PERIOD
-  tc qdisc delete dev ens33 root
+  tc qdisc delete dev $IFACE root
   echo
 elif [ $x -eq 2 ]; then
   echo "Introducing delay of" "${DELAY}""ms" "for" "${WAIT_PERIOD}""sec"
   echo
-  tc qdisc add dev ens33 root netem delay ${DELAY}ms 10ms distribution normal
+  tc qdisc add dev $IFACE root netem delay ${DELAY}ms 10ms distribution normal
   sleep $WAIT_PERIOD
-  tc qdisc delete dev ens33 root
+  tc qdisc delete dev $IFACE root
   echo
 elif [ $x -eq 3 ]; then
   echo "Introducing packet corruption of" "${CORRUPT}""%" "for" "${WAIT_PERIOD}""sec"
   echo
-  tc qdisc replace dev ens33 root netem corrupt ${CORRUPT}%
+  tc qdisc replace dev $IFACE root netem corrupt ${CORRUPT}%
   sleep $WAIT_PERIOD
-  tc qdisc delete dev ens33 root
+  tc qdisc delete dev $IFACE root
   echo
 else
   echo "Something went wrong"
   exit 1
 fi
-
-
-
